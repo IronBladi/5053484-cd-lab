7943ba700cef1b255f72b287712fe098862f61ba
Gabo Valenzuela <tj.gabriel.aramayo@gmail.com>
2026-01-22 06:20:13 -0400

    fix: configurar entorno Jest para ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,5 +3,26 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
+  // Configuración general para JS
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      }
+    }
+  },
+
+  // Configuración ESPECÍFICA para tests Jest
+  {
+    files: ["**/*.test.js"],
+    languageOptions: {
+      globals: {
+        ...globals.jest
+      }
+    }
+  }
 ]);
